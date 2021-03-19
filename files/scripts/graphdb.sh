50e6f62071391c3a61e0fa9b9cef9eb7736ca645
Nikolay Kolev <nikolay.kolev@ontotext.com>
2021-03-19 12:09:10 +0200

    Added 2m3w_ro_rw topology

--- files/scripts/graphdb.sh
+++ files/scripts/graphdb.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 set -eu
+
 #TODO: Add a function which sets a master as read only
 function waitService() {
   address=$1
@@ -48,44 +49,113 @@ waitWorkers() {
 
 linkWorkerToMaster() {
   master_address=http://$1:7200
+  master_repo=$2
+  worker_address=http://$3:7200
+  worker_repository=$4
+
+  worker_repo_endpoint="${worker_address}/repositories/${worker_repository}"
+  waitService "${worker_address}/rest/repositories"
+  waitService "${worker_address}/rest/repositories/${worker_repository}/size"
+
+  waitService "${master_address}/rest/repositories"
+  waitService "${master_address}/rest/repositories/${worker_repository}/size"
+
+  echo "Adding worker ${worker_address} as remote location"
+
+  curl ${master_address}/rest/locations -H 'Content-Type:application/json' \
+    -H 'Accept: application/json, text/plain, */*' \
+    --data-raw "{\"uri\":\"${worker_address}\",\"username\":\"\", \"password\":\"\", \"active\":\"false\"}"
+
+  echo "Linking worker with repo endpoint ${worker_repo_endpoint}"
+  curl -o response.json -sf -X POST ${master_address}/jolokia/ \
+    --header 'Content-Type: multipart/form-data' \
+    --data-raw "{
+      \"type\": \"exec\",
+      \"mbean\": \"ReplicationCluster:name=ClusterInfo/${master_repo}\",
+      \"operation\": \"addClusterNode\",
+      \"arguments\": [
+        \"${worker_repo_endpoint}\", 0, true
+      ]
+    }"
+   if grep -q '"status":200' "response.json"; then
+      echo "Linking successfull for worker $worker_address"
+  else
+      echo "Linking failed for worker ${worker_address}"
+      exit 1
+  fi
+
+  echo "Cluster linked successfully!"
+}
+
+setInstanceReadOnly() {
+  instance_address=http://$1:7200
+  repository=$2
+
+  echo "Setting instance $instance_address as readonly"
+
+  curl -o response.json -H 'content-type: application/json' -d "{\"type\":\"write\",\"mbean\":\"ReplicationCluster:name=ClusterInfo\/$repository\",\"attribute\":\"ReadOnly\",\"value\":true}" $instance_address/jolokia
+
+  if grep -q '"status":200' "response.json"; then
+      echo "Successfully set instance $instance_address as read only"
+  else
+      echo "Failed setting instance read only $instance_address"
+      exit 1
+  fi
+}
+
+setSyncPeer() {
+  instance1_address=http://$1:7200
+  instance2_address=http://$3:7200
+  instance1_repository=$2
+  instance2_repository=$4
+
+  echo "Adding sync peer $instance2_address as remote location in $instance1_address"
+
+  curl ${instance1_address}/rest/locations -H 'Content-Type:application/json' \
+    -H 'Accept: application/json, text/plain, */*' \
+    --data-raw "{\"uri\":\"${instance2_address}\",\"username\":\"\", \"password\":\"\", \"active\":\"false\"}"
+
+  echo "Setting $instance2_address as sync pear for $instance1_address"
+
+  curl -o response.json -H 'content-type: application/json' -d "{\"type\":\"exec\",\"mbean\":\"ReplicationCluster:name=ClusterInfo\/$instance1_repository\",\"operation\":\"addSyncPeer\",\"arguments\":[\"$instance2_address/repositories/$instance2_repository\",\"$instance2_address/repositories/$instance2_repository\"]}"   $instance1_address/jolokia/
+  if grep -q '"status":200' "response.json"; then
+      echo "Successfully set sync peer between $instance1_address and $instance2_address"
+  else
+      echo "Failed setting sync peer between $instance1_address and $instance2_address"
+      exit 1
+  fi
+}
+
+linkAllWorkersToMaster() {
   worker_repository=$4
   master_repo=$2
   workers_count=$3
   for (( c=1; c<=$workers_count; c++ ))
   do
     worker_address=http://graphdb-worker-$c:7200
-    worker_repo_endpoint="${worker_address}/repositories/${worker_repository}"
-
-    echo "Adding worker ${worker_address} as remote location"
-
-    curl ${master_address}/rest/locations -H 'Content-Type:application/json' \
-      -H 'Accept: application/json, text/plain, */*' \
-      --data-raw "{\"uri\":\"${worker_address}\",\"username\":\"\", \"password\":\"\", \"active\":\"false\"}"
-
-    echo "Linking worker with repo endpoint ${worker_repo_endpoint}"
-    curl -o response.json -sf -X POST ${master_address}/jolokia/ \
-      --header 'Content-Type: multipart/form-data' \
-      --data-raw "{
-        \"type\": \"exec\",
-        \"mbean\": \"ReplicationCluster:name=ClusterInfo/${master_repo}\",
-        \"operation\": \"addClusterNode\",
-        \"arguments\": [
-          \"${worker_repo_endpoint}\", 0, true
-        ]
-      }"
-     if grep -q '"status":200' "response.json"; then
-        echo "Linking successfull for worker $worker_address"
-    else
-        echo "Linking failed for worker ${worker_address}"
-        exit 1
-    fi
+    linkWorkerToMaster $1 master_repo worker_address worker_repository
   done
 
   echo "Cluster linked successfully!"
 }
-#workersCount, workerRepo
-waitWorkers $4 $5
-#mastersCount, mastersRepo
-waitMasters $2 $3
-#1 master, multiple workers. Args: master to link to, master repo, workers count, workers repo
-linkWorkerToMaster graphdb-master-1 $3 $4 $5
+
+waitAllInstances() {
+  #workersCount, workerRepo
+  waitWorkers $3 $4
+  #mastersCount, mastersRepo
+  waitMasters $1 $2
+}
+
+link_1m_3w() {
+  #masters count, master repo, workers count, worker repo
+  waitAllInstances $1 $2 $3 $4
+  #1 master, multiple workers. Args: master to link to, master repo, workers count, workers repo
+  linkAllWorkersToMaster graphdb-master-1 $2 $3 $4
+}
+
+link_2m3w_rw_ro() {
+  #1 master, multiple workers. Args: master to link to, master repo, workers count, workers repo
+  linkWorkerToMaster graphdb-master-1 $2 $3 $4
+}
+
+"$@"

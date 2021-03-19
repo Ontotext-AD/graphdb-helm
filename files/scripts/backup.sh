04f66c6b9e54dc498d6a3529621aaad935cb80a7
vasilnv <vasil_vasilev1999@abv.bg>
2021-03-19 11:59:42 +0200

    Create Backup

--- files/scripts/backup.sh
+++ files/scripts/backup.sh
@@ -1,6 +1,4 @@
-#!/usr/bin/env bash
 repo_name=$1
-
 function waitService() {
   address=$1
 
@@ -22,18 +20,4 @@ function waitService() {
 
 waitService http://graphdb-master-1:7200/rest/repositories/${repo_name}/size
 
-backupDir=$(date +'%m-%d-%Y')
-
-i=0
-while [ $i -lt 3 ]
-do
-  curl -o response.json -H 'content-type: application/json' -d "{\"type\":\"exec\", \"mbean\":\"ReplicationCluster:name=ClusterInfo\/$repo_name\", \"operation\":\"backup\", \"arguments\":[\"${backupDir}\"]}" http://graphdb-master-1:7200/jolokia/
-  if grep -q '"status":200' "response.json"; then
-    break
-  else
-    echo "curl failed"
-  fi
-  i=$((i+1))
-done
-
-#curl -H 'content-type: application/json' -d "{\"type\":\"exec\", \"mbean\":\"ReplicationCluster:name=ClusterInfo\/$repo_name\", \"operation\":\"backup\", \"arguments\":[\"${backupDir}\"]}" http://graphdb-master-1:7200/jolokia/
+curl -H 'content-type: application/json' -d '{"type":"exec", "mbean":"ReplicationCluster:name=ClusterInfo\/${repo_name}", "operation":"backup", "arguments":[null]}' http://graphdb-master-1:7200/jolokia/

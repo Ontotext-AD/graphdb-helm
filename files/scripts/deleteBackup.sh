4ac95897940d930cb39e0465bf5529ccc6a10eee
vasilnv <vasil_vasilev1999@abv.bg>
2021-03-22 20:37:49 +0200

    Fix Cleanup job

--- files/scripts/deleteBackup.sh
+++ files/scripts/deleteBackup.sh
@@ -1,9 +1,12 @@
-#!/bin/bash
 backup_dir=$1
-while [ $(ls -l ${backup_dir} | wc -l) -gt 5 ]
+backups_max_count=$2
+
+echo "num of dirs: $(ls ${backup_dir} | wc -w) in ${backup_dir}"
+
+while [ $( ls ${backup_dir} | wc -w)  -gt ${backups_max_count} ]
 do
-	toDel=$(find ${backup_dir} -maxdepth 1 -type d -printf "%T@ %p \n" | sort -n | tail -n 2 | head -n 1 | cut -d ' ' -f2)
+	toDel=$( find  ${backup_dir} -maxdepth 1 -mindepth 1 -type d -exec stat -c '%X %n' {} \; | sort -nr | tail -n 1 | cut -d ' ' -f2 )
 	echo "removing ${toDel}"
-	rm -rf ${toDel}
+	rm -rf "${toDel}"
 done
 

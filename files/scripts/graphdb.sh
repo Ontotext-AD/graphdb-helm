5e31d7edb90d1ddfab125d01dd1c5a3d941d7c40
Nikolay Kolev <nikolay.kolev@ontotext.com>
2021-06-07 16:34:51 +0300

    Make sure shell variables scopes don't mix up. Fixed muted topology

--- files/scripts/graphdb.sh
+++ files/scripts/graphdb.sh
@@ -2,12 +2,12 @@
 
 set -eu
 
-function waitService() {
-  address=$1
-  token=$2
+function waitService {
+  local address=$1
+  local token=$2
 
-  attempt_counter=0
-  max_attempts=100
+  local attempt_counter=0
+  local max_attempts=100
 
   echo "Waiting for ${address}"
   until $(curl --output /dev/null -H "Authorization: Basic ${token}" --silent --fail ${address}); do
@@ -22,10 +22,10 @@ function waitService() {
   done
 }
 
-waitMasters() {
-  masters_count=$1
-  master_repo=$2
-  token=$3
+function waitMasters {
+  local masters_count=$1
+  local master_repo=$2
+  local token=$3
 
   for (( c=1; c<=$masters_count; c++ ))
   do
@@ -36,27 +36,27 @@ waitMasters() {
   done
 }
 
-waitWorkers() {
-  workers_count=$1
-  workers_repo=$2
-  token=$3
+function waitWorkers {
+  local workers_count=$1
+  local workers_repo=$2
+  local token=$3
 
   for (( c=1; c<=$workers_count; c++ ))
   do
-    workers_address=http://graphdb-worker-$c:7200
+    local workers_address=http://graphdb-worker-$c:7200
     waitService "${workers_address}/rest/repositories" $token
     waitService "${workers_address}/rest/repositories/${workers_repo}/size" $token
   done
 }
 
-linkWorkerToMaster() {
-  master_address=http://$1:7200
-  master_repo=$2
-  worker_address=http://$3:7200
-  worker_repository=$4
-  token=$5
+function linkWorkerToMaster {
+  local master_address=http://$1:7200
+  local master_repo=$2
+  local worker_address=http://$3:7200
+  local worker_repository=$4
+  local token=$5
 
-  worker_repo_endpoint="${worker_address}/repositories/${worker_repository}"
+  local worker_repo_endpoint="${worker_address}/repositories/${worker_repository}"
   waitService "${worker_address}/rest/repositories" $token
   waitService "${worker_address}/rest/repositories/${worker_repository}/size" $token
 
@@ -86,10 +86,10 @@ linkWorkerToMaster() {
   echo "Worker linked successfully!"
 }
 
-setInstanceReadOnly() {
-  instance_address=http://$1:7200
-  repository=$2
-  token=$3
+function setInstanceReadOnly {
+  local instance_address=http://$1:7200
+  local repository=$2
+  local token=$3
 
   echo "Setting instance $instance_address as readonly"
 
@@ -103,10 +103,10 @@ setInstanceReadOnly() {
   fi
 }
 
-setInstanceMuted() {
-  instance_address=http://$1:7200
-  repository=$2
-  token=$3
+function setInstanceMuted {
+  local instance_address=http://$1:7200
+  local repository=$2
+  local token=$3
 
   echo "Setting instance $instance_address as muted"
 
@@ -120,12 +120,12 @@ setInstanceMuted() {
   fi
 }
 
-addInstanceAsRemoteLocation() {
-  master_address=http://$1:7200
-  worker_address=http://$2:7200
-  token=$3
-  username=$(echo $token | base64 -d | cut -d':' -f1)
-  password=$(echo $token | base64 -d | cut -d':' -f2)
+function addInstanceAsRemoteLocation {
+  local master_address=http://$1:7200
+  local worker_address=http://$2:7200
+  local token=$3
+  local username=$(echo $token | base64 -d | cut -d':' -f1)
+  local password=$(echo $token | base64 -d | cut -d':' -f2)
 
   echo "Adding worker $worker_address as remote location of $master_address"
 
@@ -139,12 +139,12 @@ addInstanceAsRemoteLocation() {
   fi
 }
 
-setSyncPeer() {
-  instance1_address=http://$1:7200
-  instance2_address=http://$3:7200
-  instance1_repository=$2
-  instance2_repository=$4
-  token=$5
+function setSyncPeer {
+  local instance1_address=http://$1:7200
+  local instance2_address=http://$3:7200
+  local instance1_repository=$2
+  local instance2_repository=$4
+  local token=$5
 
   addInstanceAsRemoteLocation $1 $3 $token
 
@@ -157,29 +157,32 @@ setSyncPeer() {
       echo "Failed setting sync peer between $instance1_address and $instance2_address"
       exit 1
   fi
+
+  setNodeID $1 $2 $token
+  setNodeID $3 $4 $token
 }
 
-linkAllWorkersToMaster() {
-  worker_repository=$4
-  master_repo=$2
-  workers_count=$3
-  token=$5
+function linkAllWorkersToMaster {
+  local worker_repository=$4
+  local master_repo=$2
+  local workers_count=$3
+  local token=$5
 
   for (( c=1; c<=$workers_count; c++ ))
   do
-    worker_address=graphdb-worker-$c
+    local worker_address=graphdb-worker-$c
     linkWorkerToMaster $1 $master_repo $worker_address $worker_repository $token
   done
 
   echo "Cluster linked successfully!"
 }
 
-unlinkWorker() {
-  master_repo=$1
-  master_address=$2
-  worker_address=$3
-  worker_repo=$4
-  token=$5
+function unlinkWorker {
+  local master_repo=$1
+  local master_address=$2
+  local worker_address=$3
+  local worker_repo=$4
+  local token=$5
 
   echo "Unlinking $worker_address from $master_address"
   curl -X 'DELETE' "http://$master_address:7200/graphdb/rest/cluster/masters/$master_repo/workers?masterLocation=local" -H "Authorization: Basic $token" --data-urlencode "workerURL=http://$worker_address:7200/repositories/$worker_repo"
@@ -192,28 +195,28 @@ unlinkWorker() {
   fi
 }
 
-unlinkDownScaledInstances() {
-  master_repo=$1
-  masters_count=$2
-  workers_count=$3
-  worker_repo=$4
-  token=$5
+function unlinkDownScaledInstances {
+  local master_repo=$1
+  local masters_count=$2
+  local workers_count=$3
+  local worker_repo=$4
+  local token=$5
 
   for (( c=1; c<=$masters_count; c++ ))
   do
-    master_address=graphdb-master-$c
+    local master_address=graphdb-master-$c
     curl -o response.json -H 'content-type: application/json'  -H "Authorization: Basic $token" -d "{\"type\":\"read\",\"mbean\":\"ReplicationCluster:name=ClusterInfo\/$master_repo\",\"attribute\":\"NodeStatus\"}"   http://$master_address:7200/jolokia/
-    linked_workers_count=$(grep -ow ON "response.json" | wc -l)
-    missing_workers_count=$(grep -ow ON "response.json" | wc -l)
+    local linked_workers_count=$(grep -ow ON "response.json" | wc -l)
+    local missing_workers_count=$(grep -ow ON "response.json" | wc -l)
 
     if $linked_workers_count != $workers_count ; then
       echo "The cluster has instances that are not connected, but they should be. Can't determine workers which must be disconnected from the cluster, please do it manually!"
     else
-      worker_to_be_unlinked=$linked_workers_count+$missing_workers_count
+      local worker_to_be_unlinked=$linked_workers_count+$missing_workers_count
       for (( x=1; x<=$missing_workers_count; x++ ))
       do
         unlinkWorker $master_repo $master_address graphdb-worker-$worker_to_be_unlinked $worker_repo $token
-        worker_to_be_unlinked=$worker_to_be_unlinked-1
+        local worker_to_be_unlinked=$worker_to_be_unlinked-1
       done
     fi
     linkWorkerToMaster $1 $master_repo $worker_address $worker_repository $token
@@ -222,19 +225,33 @@ unlinkDownScaledInstances() {
   echo "Cluster linked successfully!"
 }
 
-waitAllInstances() {
+function waitAllInstances {
   #workersCount, workerRepo, token
   waitWorkers $3 $4 $5
   #mastersCount, mastersRepo, token
   waitMasters $1 $2 $5
 }
 
-link_1m_3w() {
+function link_1m_3w {
   #masters count, master repo, workers count, worker repo, token
   waitAllInstances $1 $2 $3 $4 $5
 
   #1 master, multiple workers. Args: master to link to, master repo, workers count, workers repo, token
   linkAllWorkersToMaster graphdb-master-1 $2 $3 $4 $5
+}
+
+function setNodeID {
+  local instance_address=http://$1:7200
+  local instance_repository=$2
+  local token=$3
+  echo "Setting NodeID for $instance_address"
+  curl -o response.json -H 'content-type: application/json' -H "Authorization: Basic $token" -d "{\"type\":\"write\",\"mbean\":\"ReplicationCluster:name=ClusterInfo\/$instance_repository\",\"attribute\":\"NodeID\",\"value\":\"$instance_address/repositories/$instance_repository\"}" $instance_address/jolokia/
+  if grep -q '"status":200' "response.json"; then
+      echo "Successfully set NodeID for $instance_address"
+  else
+      echo "Failed setting NodeID for $instance_address"
+      exit 1
+  fi
 }
 
 "$@"

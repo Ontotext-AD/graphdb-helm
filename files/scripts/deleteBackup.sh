2d3c44c93c17b355228f55a88bcdb9a987c97d6b
nikolaykolev <nikolay.kolev@sirma.bg>
2021-07-01 11:00:28 +0300

    Backup/restore fix, other improvements (#7)
    * Work on backups
    
    * Fixed backup/restore
    
    * Bumped version
    
    * Revert default configs
    
    * Fixed standalone deployment
    
    * Fixed wrong volume mount name
    
    * remove hook-success deletion policy for crons
    
    Co-authored-by: Nikolay Kolev <nikolay.kolev@ontotext.com>
    Co-authored-by: Yasen Terziivanov <yasen.terziivanov@ontotext.com>

--- files/scripts/deleteBackup.sh
+++ files/scripts/deleteBackup.sh
@@ -1,20 +1,22 @@
 backup_dir=$1
-backups_max_count=$2
-backup_days_max_count=$3
+repository_name=$2
+backups_max_count=$3
+backup_days_max_count=$4
 
-echo "Number of dirs: $(ls ${backup_dir} | wc -w) in ${backup_dir}"
+echo "Number of dirs: $(ls $backup_dir | grep $repository_name | wc -w) in $backup_dir"
 
-if [ ! -z ${backup_days_max_count} ] && [ ! -z $(find ${backup_dir} -maxdepth 1 -mindepth 1 -type d -mtime +${backup_days_max_count}) ]
+if [ ! -z $backup_days_max_count ] && [ ! -z $(find $backup_dir -maxdepth 1 -mindepth 1 -type d -mtime +$backup_days_max_count -name "$repository_name*") ]
 then
-  find ${backup_dir} -maxdepth 1 -mindepth 1 -type d -mtime +${backup_days_max_count} -exec rm -rf {} \;
-  echo "Successfully removed old backups"
+  echo "Removing backups $(find ${backup_dir} -maxdepth 1 -mindepth 1 -type d -mtime +${backup_days_max_count} -name "$repository_name*")"
+  find ${backup_dir} -maxdepth 1 -mindepth 1 -type d -mtime +${backup_days_max_count} -name "$repository_name*" -exec rm -rf {} \;
+  echo "Successfully removed old backups for repository $repository_name"
 fi
 
 if [ ! -z ${backups_max_count} ]
 then
   while [ $( ls ${backup_dir} | wc -w)  -gt ${backups_max_count} ]
   do
-    toDel=$( find  ${backup_dir} -maxdepth 1 -mindepth 1 -type d -exec stat -c '%X %n' {} \; | sort -nr | tail -n 1 | cut -d ' ' -f2 )
+    toDel=$( find  ${backup_dir} -maxdepth 1 -mindepth 1 -type d -name "$repository_name*" -exec stat -c '%X %n' {} \; | sort -nr | tail -n 1 | cut -d ' ' -f2 )
     echo "Removing ${toDel}"
     rm -rf "${toDel}"
   done

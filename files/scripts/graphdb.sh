b4dd8b9d85655deb9eed947c2c39cd576ae452e6
sukhumi <lubenkaraslavov@abv.bg>
2022-07-21 19:05:06 +0300

    formatted graphdb.sh

--- files/scripts/graphdb.sh
+++ files/scripts/graphdb.sh
@@ -4,32 +4,31 @@ set -eu
 function createCluster {
   waitAllNodes $1 $3
   local configLocation=$2
-  local token=$3
+  local authToken=$3
   local timeout=$4
   echo "Creating cluster"
-  curl -o response.json -isSL -m $timeout -X POST --header "Authorization: Basic ${token}" --header 'Content-Type: application/json' --header 'Accept: */*' -d @"$configLocation" http://graphdb-node-0.graphdb-node:7200/rest/cluster/config
-    if grep -q 'HTTP/1.1 201' "response.json"; then
-        echo "Cluster creation successful!"
-    else if grep -q 'Cluster already exists.\|HTTP/1.1 409' "response.json" ; then
-            echo "Cluster already exists"
-         else
-            echo "Cluster creation failed, received response:"
-            cat response.json
-            echo
-            exit 1
-        fi
-    fi
+  curl -o response.json -isSL -m $timeout -X POST --header "Authorization: Basic ${authToken}" --header 'Content-Type: application/json' --header 'Accept: */*' -d @"$configLocation" http://graphdb-node-0.graphdb-node:7200/rest/cluster/config
+  if grep -q 'HTTP/1.1 201' "response.json"; then
+    echo "Cluster creation successful!"
+  elif grep -q 'Cluster already exists.\|HTTP/1.1 409' "response.json" ; then
+    echo "Cluster already exists"
+  else
+    echo "Cluster creation failed, received response:"
+    cat response.json
+    echo
+    exit 1
+  fi
 }
 
 function waitService {
   local address=$1
-  local token=$2
+  local authToken=$2
 
   local attempt_counter=0
   local max_attempts=100
 
   echo "Waiting for ${address}"
-  until $(curl --output /dev/null -fsSL -m 5 -H "Authorization: Basic ${token}" --silent --fail ${address}); do
+  until $(curl --output /dev/null -fsSL -m 5 -H "Authorization: Basic ${authToken}" --silent --fail ${address}); do
     if [[ ${attempt_counter} -eq ${max_attempts} ]];then
       echo "Max attempts reached"
       exit 1
@@ -43,13 +42,13 @@ function waitService {
 
 function waitAllNodes {
   local node_count=$1
-  local token=$2
+  local authToken=$2
 
   for (( c=$node_count; c>0; c ))
   do
     c=$((c-1))
     local node_address=http://graphdb-node-$c.graphdb-node:7200
-    waitService "${node_address}/rest/repositories" "$token"
+    waitService "${node_address}/rest/repositories" "$authToken"
   done
 }
 

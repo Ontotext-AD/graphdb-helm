40e099781c3dbda3f56496c15f89e9482ebc132a
Luben Karaslavov <40731850+Sukhumi@users.noreply.github.com>
2023-03-22 10:17:43 +0200

    Various improvements to the chart gdb 7987 7985 7984 7978 and more (#55)
    - Added .gitignore
    - Added configurable security context for both the node and cluster-proxy statefulsets and all the jobs
    - Added extraEnv, extraVolumes and extraVolumeMounts to the statefulsets
    - Added an optional PV/PVC to the cluster-proxy to properly preserve logs (enabled by default)
    - Changed the provision user credentials to be used through a secret instead of rendering inside the jobs
    - Changed the logback.xml and graphdb.properties provisioning to work even if such are already present
    - Changed the graphdb-cluster-config-configmap map to not render when there is no cluster
    - Changed the default values of nodeSelector, affinity, tolerations and topologySpreadConstraints to be a part of the values.yaml file instead of inside the statefulsets
    - Updated default clusterConfig.electionMinTimeout and clusterConfig.electionRangeTimeout to the current GraphDB defaults

--- files/scripts/graphdb.sh
+++ files/scripts/graphdb.sh
@@ -2,10 +2,10 @@
 set -eu
 
 function createCluster {
-  waitAllNodes $1 $3
+  waitAllNodes $1
   local configLocation=$2
-  local authToken=$3
-  local timeout=$4
+  local authToken=$PROVISION_USER_AUTH_TOKEN
+  local timeout=$3
   echo "Creating cluster"
   curl -o response.json -isSL -m $timeout -X POST --header "Authorization: Basic ${authToken}" --header 'Content-Type: application/json' --header 'Accept: */*' -d @"$configLocation" http://graphdb-node-0.graphdb-node:7200/rest/cluster/config
   if grep -q 'HTTP/1.1 201' "response.json"; then
@@ -22,7 +22,7 @@ function createCluster {
 
 function waitService {
   local address=$1
-  local authToken=$2
+  local authToken=$PROVISION_USER_AUTH_TOKEN
 
   local attempt_counter=0
   local max_attempts=100
@@ -42,20 +42,19 @@ function waitService {
 
 function waitAllNodes {
   local node_count=$1
-  local authToken=$2
 
   for (( c=$node_count; c>0; c ))
   do
     c=$((c-1))
     local node_address=http://graphdb-node-$c.graphdb-node:7200
-    waitService "${node_address}/rest/repositories" "$authToken"
+    waitService "${node_address}/rest/repositories"
   done
 }
 
 function createRepositoryFromFile {
-  waitAllNodes $1 $3
+  waitAllNodes $1
   local repositoriesConfigsLocation=$2
-  local authToken=$3
+  local authToken=$PROVISION_USER_AUTH_TOKEN
   local timeout=60
   echo "Creating repositories"
   local success=true
